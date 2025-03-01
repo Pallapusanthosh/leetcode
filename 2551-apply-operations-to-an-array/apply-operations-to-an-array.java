f2f4a84c8367d80d4129ae679e339d09aedcba5a
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-03-01 08:45:33 +0530

    Time: 1 ms (96.37%) | Memory: 43.3 MB (97.15%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -1,37 +1,30 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
         int n = nums.length;
-        int i = 0;
-        while(i<n-1){
-            if(nums[i] == nums[i+1]){
-                nums[i] *=2;
-                nums[i+1] = 0;
-                i++;
-            }
-            i++;
-        }
-        int count =0;
-        List<Integer> ar = new ArrayList<>();
-        for(int num:nums){
-            if(num!=0){
-                ar.add(num);
-            }
-            else{
-                count+=1;
-            }
-            
-        }
-        while(count > 0){
-            ar.add(0);
-            count -=1;
-        }
-        int[] result = new int[ar.size()];
-        int l =0;
-        for(int num:ar){
-            result[l] = num;
-            l++;
-        }
-        return result;
         
+        // Apply operations
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+        
+        // Shift zeros to the end (in-place)
+        int nonZeroIdx = 0;
+        
+        // Move all non-zero elements to the front
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIdx++] = nums[i];
+            }
+        }
+        
+        // Fill the remaining positions with zeros
+        while (nonZeroIdx < n) {
+            nums[nonZeroIdx++] = 0;
+        }
+        
+        return nums;
     }
 }
