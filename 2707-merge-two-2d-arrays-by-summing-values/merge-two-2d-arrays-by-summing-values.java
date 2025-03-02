08821683e952fd21f27489447ae93483744c456c
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-03-02 11:55:53 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (47.90%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,56 +1,28 @@
-import java.util.*;
-
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        List<ArrayList<Integer>> res = new ArrayList<>();
-        int n1 = nums1.length, n2 = nums2.length;
-        int i = 0, j = 0;
-
-        while (i < n1 && j < n2) {
-            if (nums1[i][0] == nums2[j][0]) {
-                ArrayList<Integer> subl = new ArrayList<>();
-                subl.add(nums1[i][0]);
-                subl.add(nums1[i][1] + nums2[j][1]);
-                res.add(subl);
+        int n1=nums1.length,n2=nums2.length,i=0,j=0;
+        ArrayList<int []>ans=new ArrayList();
+        while(i<n1&& j<n2){
+            if(nums1[i][0]==nums2[j][0]){
+                ans.add(new int[]{nums1[i][0],nums1[i][1]+nums2[j][1]});
                 i++;
                 j++;
-            } else if (nums1[i][0] < nums2[j][0]) {
-                ArrayList<Integer> subl = new ArrayList<>();
-                subl.add(nums1[i][0]);
-                subl.add(nums1[i][1]);
-                res.add(subl);
+            }
+            else if(nums1[i][0]<nums2[j][0]){
+                ans.add(nums1[i]);
                 i++;
-            } else {
-                ArrayList<Integer> subl = new ArrayList<>();
-                subl.add(nums2[j][0]);
-                subl.add(nums2[j][1]);
-                res.add(subl);
+            }
+            else{
+                ans.add(nums2[j]);
                 j++;
             }
         }
-
-        while (i < n1) {
-            ArrayList<Integer> subl = new ArrayList<>();
-            subl.add(nums1[i][0]);
-            subl.add(nums1[i][1]);
-            res.add(subl);
-            i++;
+        while(i<n1){
+            ans.add(nums1[i++]);
         }
-
-        while (j < n2) {
-            ArrayList<Integer> subl = new ArrayList<>();
-            subl.add(nums2[j][0]);
-            subl.add(nums2[j][1]);
-            res.add(subl);
-            j++;
+        while(j<n2){
+            ans.add(nums2[j++]);
         }
-
-        int[][] m = new int[res.size()][2];
-        for (int k = 0; k < res.size(); k++) {
-            m[k][0] = res.get(k).get(0);
-            m[k][1] = res.get(k).get(1);
-        }
-
-        return m;
+        return ans.toArray(new int[ans.size()][]);
     }
 }
