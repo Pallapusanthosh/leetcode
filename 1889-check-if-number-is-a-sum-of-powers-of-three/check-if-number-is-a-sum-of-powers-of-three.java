b3dfea3cdf9294f515b7877c18766625329d912b
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-03-04 06:44:54 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (22.96%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,25 +1,15 @@
 class Solution {
     public boolean checkPowersOfThree(int n) {
-        int[] threepower = new int[16];
-        for(int i =0;i<16;i++){
-            threepower[i] = (int) Math.pow(3,i);
+        int rem;
+        while(n >= 3){
+            rem = n%3;
+            n = n/3;
+            if(rem!=0 && rem!=1){
+                return false;
+            }
         }
-        List<Integer> res = new ArrayList<>();
-        if(findSubset(threepower,0,n,res)){
+        if(n == 0 || n == 1){
             return true;
-        }
-        return false;
-
-    }
-     private static boolean findSubset(int[] nums, int index, int target, List<Integer> result) {
-        if (target == 0) return true; // Found a valid subset
-
-        for (int i = index; i < nums.length; i++) {
-            if (nums[i] > target) break; // Prune if number is too large
-
-            result.add(nums[i]); // Pick the number
-            if (findSubset(nums, i + 1, target - nums[i], result)) return true; // Recur
-            result.remove(result.size() - 1); // Backtrack
         }
         return false;
     }
