4eef5005c0cebe07da62cd8cbe7ec5a997c87500
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-03-05 10:40:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (16.39%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,5 +1,7 @@
 class Solution {
     public long coloredCells(int n) {
+        //cal first the vertical and horizontol through centre  (n + (n - 1)) * 2 - 1)
+        //cal the no of boxes at each sectors * 4
         if(n == 1){
             return 1;
         }
