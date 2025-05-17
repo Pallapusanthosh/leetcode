ac75c51e63e6b008488c04abd461309d3269cafd
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-05-17 07:10:41 +0530

    Time: 1 ms (21.22%) | Memory: 42.1 MB (30.03%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -7,10 +7,11 @@ class Solution {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        // Since the problem is only about 0s, 1s, and 2s, you don't need to extract keys
+        
         int k = 0;
         for (int i = 0; i <= 2; i++) {
             int count = map.getOrDefault(i, 0);
+            if(count == 0) continue;
             for (int j = 0; j < count; j++) {
                 nums[k++] = i;
             }
