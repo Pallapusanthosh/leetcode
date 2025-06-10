295cc64afa730d62b7850314ffd65bec2f5a27c9
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-06-10 16:01:00 +0530

    Time: 10 ms (50.89%) | Memory: 49.2 MB (44.54%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,29 +1,24 @@
 class Solution {
       
     public int change(int amount, int[] coins) {
-        int[][] memo = new int[coins.length][amount+1];
+        int[][] memo = new int[coins.length+1][amount+1];
         
-        for(int i = 0 ; i < coins.length ; i++){
-            for(int j = 0 ; j<amount+1 ; j++){
-                memo[i][j] = -1;
+       
+        for (int i = 0; i <= coins.length; i++) {
+            memo[i][0] = 1;
+        }
+        for(int i = 1 ; i <= coins.length ; i++){
+            for(int j = 1 ; j<amount+1 ; j++){
+                int nottake = memo[i-1][j];
+                int take =0;
+                if(coins[i-1] <= j){
+                        take =  memo[i][j-coins[i-1]];
+                }
+                memo[i][j] = take + nottake;
             }
         }
-        
-        return f(coins.length -1 , amount , coins , memo);
+    
+        return memo[coins.length ][amount];
     }
-    public int f(int n , int a,int[] coins ,int[][] memo){
-        if(a == 0){
-            return 1;
-        }
-        if(n < 0) return 0;
-        if(memo[n][a]!=-1) return memo[n][a];
-        // if(coins[n] > a) return;
-
-        int nottake = f(n-1,a,coins,memo);
-        int take =0;
-        if(coins[n] <= a){
-             take =  f(n,a-coins[n],coins,memo);
-        }
-        return memo[n][a] = take + nottake;
-    }
+  
 }
