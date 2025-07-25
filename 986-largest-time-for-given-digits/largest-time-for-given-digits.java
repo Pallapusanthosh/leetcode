ebe1278269279872364cf302e1405092e33f8e7e
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-07-25 22:41:08 +0530

    Time: 8 ms (51.31%) | Memory: 41.6 MB (72.28%) - LeetSync

---
+++ 986-largest-time-for-given-digits/largest-time-for-given-digits.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public String largestTimeFromDigits(int[] arr) {
+        List<List<Integer>> sol = new ArrayList<>();
+        back(arr, 0, sol);
+
+        int maxTime = -1;
+        for (List<Integer> ar : sol) {
+            int hh = ar.get(0) * 10 + ar.get(1);
+            int mm = ar.get(2) * 10 + ar.get(3);
+            if (hh < 24 && mm < 60) {
+                int totalMinutes = hh * 60 + mm;
+                maxTime = Math.max(maxTime, totalMinutes);
+            }
+        }
+
+        if (maxTime == -1) return "";
+
+        int max_hh = maxTime / 60;
+        int max_mm = maxTime % 60;
+        return String.format("%02d:%02d", max_hh, max_mm);
+    }
+
+    void back(int[] a, int i, List<List<Integer>> sol) {
+        if (i == a.length) {
+            List<Integer> cur = new ArrayList<>();
+            for (int x : a) cur.add(x);
+            sol.add(cur);
+            return;
+        }
+        for (int j = i; j < a.length; j++) {
+            swap(a, i, j);
+            back(a, i + 1, sol);
+            swap(a, i, j);
+        }
+    }
+
+    void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+}
