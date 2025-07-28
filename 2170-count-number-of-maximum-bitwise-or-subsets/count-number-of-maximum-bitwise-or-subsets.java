9dd08ed94bd8f95cf13b7b211ea7c56ce92121b4
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-07-28 12:48:58 +0530

    Time: 13 ms (33.05%) | Memory: 45 MB (18.76%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,28 +1,32 @@
-class Solution {
-    public int count = 0;
+import java.util.*;
 
+class Solution {
     public int countMaxOrSubsets(int[] nums) {
         int maxOr = 0;
-
         for (int num : nums) {
             maxOr |= num;
         }
-
-
-        backtrack(0, 0, nums, maxOr);
-        return count;
+        Map<String, Integer> memo = new HashMap<>();
+        return dfs(0, 0, nums, maxOr, memo);
     }
 
-    void backtrack(int index, int currentOr, int[] nums, int maxOr) {
-        if (index == nums.length) {
-            if (currentOr == maxOr) {
-                count++;
-            }
-            return;
+    int dfs(int i, int curOr, int[] nums, int maxOr, Map<String, Integer> memo) {
+        if (i == nums.length) {
+            return curOr == maxOr ? 1 : 0;
         }
 
-        backtrack(index + 1, currentOr | nums[index], nums, maxOr);
+        String key = i + "," + curOr;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
 
-        backtrack(index + 1, currentOr, nums, maxOr);
+        // Include nums[i]
+        int include = dfs(i + 1, curOr | nums[i], nums, maxOr, memo);
+        // Exclude nums[i]
+        int exclude = dfs(i + 1, curOr, nums, maxOr, memo);
+
+        int total = include + exclude;
+        memo.put(key, total);
+        return total;
     }
 }
