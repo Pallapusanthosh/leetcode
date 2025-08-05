f8fc6964f763a74570af7dcaa8a0b764cd0ba880
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-08-05 11:17:25 +0530

    Time: 2188 ms (5.00%) | Memory: 56.2 MB (49.79%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+
+class Solution {
+    Random rand = new Random();
+
+    public int[] sortArray(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void quickSort(int[] arr, int low, int high) {
+        while (low < high) {
+            int pivotIndex = randomizedPartition(arr, low, high);
+
+            // Tail call optimization – recurse on smaller side
+            if (pivotIndex - low < high - pivotIndex) {
+                quickSort(arr, low, pivotIndex - 1);
+                low = pivotIndex + 1;
+            } else {
+                quickSort(arr, pivotIndex + 1, high);
+                high = pivotIndex - 1;
+            }
+        }
+    }
+
+    private int randomizedPartition(int[] arr, int low, int high) {
+        int pivotIndex = rand.nextInt(high - low + 1) + low;
+        swap(arr, pivotIndex, high);
+        return partition(arr, low, high);
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
