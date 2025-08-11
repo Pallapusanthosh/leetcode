1b2abfaa0288933f5b839d67edc87afec25b360d
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-08-11 12:10:39 +0530

    Time: 1 ms (62.29%) | Memory: 46 MB (6.58%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        //bfs 
-        int m =graph.length;
-        int n= graph[0].length;
-        //graph given as arraylist;
-        //means at 0 index ele have an edge in graph[0];
-        int[] visited =new int[m];
-        int[] color = new int[m];
-         for (int i = 0; i < m; i++) {
+        int m = graph.length;
+        int[] visited = new int[m];
+        int[] color = new int[m]; // 0 = uncolored, 1 & 2 = two colors
+
+        for (int i = 0; i < m; i++) {
             if (visited[i] == 0) {
                 if (!bfs(graph, visited, color, i)) {
                     return false;
@@ -16,30 +13,22 @@ class Solution {
         }
         return true;
     }
-    boolean bfs(int[][] graph , int[] visited , int[] color , int start ){
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.add(new int[]{start,1});
-        // int color = 1;
-        while(!q.isEmpty()){
-            int[] curnode = q.poll();
-            int node = curnode[0];
-            int col = curnode[1];
-            visited[node] = 1;
-            color[node] = col;
-           
-            int size = graph[node].length;
-            for(int i = 0;i<size;i++){
-                int child = graph[node][i];
-                int childcol = col == 1 ? 2 : 1;
-                if(visited[child]==0){
-                    q.add(new int[]{child,childcol});
-                }
-                else{
-                    int childcol2 = color[child];
-                    if(col == childcol2){
-                        return false;
-                    }
+
+    boolean bfs(int[][] graph, int[] visited, int[] color, int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = 1;
+        color[start] = 1;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (int neighbor : graph[node]) {
+                if (visited[neighbor] == 0) {
+                    visited[neighbor] = 1;
+                    color[neighbor] = 3 - color[node]; // toggle between 1 and 2
+                    q.add(neighbor);
+                } else if (color[neighbor] == color[node]) {
+                    return false; // same color on both ends of an edge → not bipartite
                 }
             }
         }
