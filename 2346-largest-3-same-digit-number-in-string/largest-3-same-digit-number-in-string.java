54a3d234cad6f2644f421b877e76c205d573f74d
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-08-14 22:00:01 +0530

    Time: 9 ms (16.18%) | Memory: 42.1 MB (64.57%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -4,15 +4,21 @@ class Solution {
         int sol = -1;
         for(int i = 0; i< num.length() -2;i++){
             if(num.charAt(i) == num.charAt(i+1) && num.charAt(i) == num.charAt(i+2)){
-                int num1 =(int) num.charAt(i) -'0';
-               sol = Math.max(sol , Integer.parseInt("" + num1 +""+ num1 +"" + num1  ));
+                sol = Math.max(sol , (int) num.charAt(i) - '0');
             }
         }
-        if(Integer.toString(sol).equals("0")){
+        if(sol == 0){
             return "000";
         }else if(sol == -1){
             return "";
+        }else{
+            return ""+ sol +""+ sol +""+ sol;
         }
-        return Integer.toString(sol);
+        // if(Integer.toString(sol).equals("0")){
+        //     return "000";
+        // }else if(sol == -1){
+        //     return "";
+        // }
+        // return Integer.toString(sol);
     }
 }
