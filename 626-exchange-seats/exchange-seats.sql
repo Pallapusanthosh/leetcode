c033d3a233460fb9546a712a70066e1fefb49179
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-08-29 17:56:10 +0530

    Time: 372 ms (27.82%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,9 +1,17 @@
-SELECT 
-    CASE 
-        WHEN id % 2 = 1 AND id + 1 <= (SELECT MAX(id) FROM seat) THEN id + 1
-        WHEN id % 2 = 0 THEN id - 1
-        ELSE id
-    END AS id,
-    student
-FROM seat
-ORDER BY id;
+-- SELECT 
+--     CASE 
+--         WHEN id % 2 = 1 AND id + 1 <= (SELECT MAX(id) FROM seat) THEN id + 1
+--         WHEN id % 2 = 0 THEN id - 1
+--         ELSE id
+--     END AS id,
+--     student
+-- FROM seat
+-- ORDER BY id;
+select 
+  case 
+    when id %2 =0 then id -1
+    when id%2 =1 and id+1 <=(select max(id) from seat) then id +1
+    else id
+  end as id,
+
+student from seat order by id asc;
