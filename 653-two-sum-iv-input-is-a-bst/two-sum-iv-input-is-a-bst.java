9f26dd56dfdf66b47493ddd922df6b1e35755447
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-08-31 00:50:17 +0530

    Time: 1 ms (100.00%) | Memory: 45.8 MB (8.03%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -15,23 +15,54 @@
  */
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        //idea is to do inordertraversal if we already found k-cur root then it is true;
-        HashSet<Integer> set = new HashSet<>();
-        boolean[] ans = new boolean[] {false};
-        // ans[0] = false;
-        inorder(root,k,set,ans);
-        return ans[0];
-    }
-    void inorder(TreeNode root , int k,HashSet<Integer> set,boolean[] ans){
-        if(root == null)return;
-        if(ans[0]) return;
+    //makeing use of bst means a take an ele and found it diff and search for the diff in the bst searching in bst takes o(h) logn t is helpful and there is no extra space too
+    return dfs(root,root,k);
 
-        inorder(root.left,k,set,ans);
-        if(set.contains(root.val)){
-            ans[0] = true;
-            return;
+    }
+    boolean dfs(TreeNode curnode , TreeNode root, int k){
+        if(curnode==null)return false; 
+        int diff = k - curnode.val;
+        int curval = curnode.val;
+        if(diff!=curval && search(root,diff))return true;
+
+        return dfs(curnode.left,root,k) || dfs(curnode.right,root,k);
+        
+        
+    }
+    boolean search(TreeNode root,int diff){
+        while(root!=null){
+            if(root.val == diff){
+                return true;
+            }
+            else if(root.val < diff){
+                root = root.right;
+            }
+            else{
+                root = root.left;
+            }
         }
-        set.add(k - root.val);
-        inorder(root.right,k,set,ans);
+        return false;
     }
 }
+
+
+
+
+ //     //idea is to do inordertraversal if we already found k-cur root then it is true;
+    //     HashSet<Integer> set = new HashSet<>();
+    //     boolean[] ans = new boolean[] {false};
+    //     // ans[0] = false;
+    //     inorder(root,k,set,ans);
+    //     return ans[0];
+    // }
+    // void inorder(TreeNode root , int k,HashSet<Integer> set,boolean[] ans){
+    //     if(root == null)return;
+    //     if(ans[0]) return;
+
+    //     inorder(root.left,k,set,ans);
+    //     if(set.contains(root.val)){
+    //         ans[0] = true;
+    //         return;
+    //     }
+    //     set.add(k - root.val);
+    //     inorder(root.right,k,set,ans);
